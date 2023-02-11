70dae1d75ad88b47d90abe36b934609249b31586
PabloGarciaJC <pablogarciajc@hotmail.com>
2023-02-11 01:30:51 +0100

    actualizacion

--- database/pablogarciajc_red_social.sql
+++ database/pablogarciajc_red_social.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 08-02-2023 a las 10:06:54
+-- Tiempo de generación: 11-02-2023 a las 01:30:01
 -- Versión del servidor: 5.7.36
 -- Versión de PHP: 7.4.26
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `comments`
 --
 -- Creación: 12-12-2022 a las 02:23:08
--- Última actualización: 08-02-2023 a las 02:51:55
+-- Última actualización: 11-02-2023 a las 00:27:09
 --
 
 CREATE TABLE `comments` (
@@ -45,14 +45,13 @@ CREATE TABLE `comments` (
 --
 -- Estructura de tabla para la tabla `followers`
 --
--- Creación: 12-12-2022 a las 02:32:29
--- Última actualización: 08-02-2023 a las 02:51:59
+-- Creación: 10-02-2023 a las 20:00:12
+-- Última actualización: 11-02-2023 a las 00:27:38
 --
 
 CREATE TABLE `followers` (
   `id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
-  `notification_id` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `seguido` int(11) NOT NULL,
   `aprobada` tinyint(1) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -65,7 +64,7 @@ CREATE TABLE `followers` (
 -- Estructura de tabla para la tabla `likes`
 --
 -- Creación: 12-12-2022 a las 02:26:30
--- Última actualización: 08-02-2023 a las 04:29:42
+-- Última actualización: 09-02-2023 a las 17:31:50
 --
 
 CREATE TABLE `likes` (
@@ -82,7 +81,7 @@ CREATE TABLE `likes` (
 -- Estructura de tabla para la tabla `migrations`
 --
 -- Creación: 12-12-2022 a las 02:18:58
--- Última actualización: 12-12-2022 a las 02:18:59
+-- Última actualización: 10-02-2023 a las 20:00:12
 --
 
 CREATE TABLE `migrations` (
@@ -104,7 +103,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (6, '2022_09_15_235141_create_comments_table', 3),
 (7, '2022_09_15_234423_create_likes_table', 4),
 (8, '2022_11_14_044728_create_notifications_table', 5),
-(9, '2022_11_04_085215_create_followers_table', 6);
+(9, '2022_11_04_085215_create_followers_table', 6),
+(10, '2023_02_10_195320_eliminar_columna_followers', 7);
 
 -- --------------------------------------------------------
 
@@ -112,7 +112,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 -- Estructura de tabla para la tabla `notifications`
 --
 -- Creación: 12-12-2022 a las 02:27:06
--- Última actualización: 08-02-2023 a las 02:52:12
+-- Última actualización: 11-02-2023 a las 00:27:17
 --
 
 CREATE TABLE `notifications` (
@@ -132,7 +132,7 @@ CREATE TABLE `notifications` (
 -- Estructura de tabla para la tabla `password_resets`
 --
 -- Creación: 12-12-2022 a las 02:18:58
--- Última actualización: 01-01-2023 a las 11:20:19
+-- Última actualización: 11-02-2023 a las 00:27:54
 -- Última revisión: 12-12-2022 a las 02:18:58
 --
 
@@ -170,7 +170,7 @@ CREATE TABLE `personal_access_tokens` (
 -- Estructura de tabla para la tabla `publications`
 --
 -- Creación: 12-12-2022 a las 02:20:51
--- Última actualización: 08-02-2023 a las 04:34:11
+-- Última actualización: 11-02-2023 a las 00:28:04
 --
 
 CREATE TABLE `publications` (
@@ -188,7 +188,7 @@ CREATE TABLE `publications` (
 -- Estructura de tabla para la tabla `users`
 --
 -- Creación: 08-12-2022 a las 00:53:20
--- Última actualización: 08-02-2023 a las 04:19:51
+-- Última actualización: 10-02-2023 a las 21:02:49
 --
 
 CREATE TABLE `users` (
@@ -216,14 +216,14 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `alias`, `nombre`, `apellido`, `pais`, `direccion`, `empresa`, `cargo`, `movil`, `email`, `fotoPerfil`, `password`, `sobreMi`, `conectado`, `remember_token`, `created_at`, `updated_at`) VALUES
-(60, 'admin', 'admin', NULL, NULL, NULL, NULL, NULL, NULL, 'admin@admin.com', 'userDefault.png', '$2y$10$vkRxKWQlwqt.zzPvCWqRP.mCaAh58BdJ5dBoWQQVMLAhVyjfUCcji', NULL, 1, NULL, '2023-02-08 03:01:32', '2023-02-08 03:01:32'),
-(68, 'alias0', 'nombre0', 'apellido0', 'Lorem Ipsum0', 'Lorem Ipsum0', 'Lorem Ipsum0', 'Lorem Ipsum0', '(+58) 123456120', 'Lorem Ipsum@hotmail.com0', 'profile0.png', '$2y$10$rMLzduWV/Yv0BOwgwfqb3emfhQuliaHq7yUDv3Y5.hDsU2xrtPbVG0', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th0', 0, NULL, NULL, NULL),
-(69, 'alias1', 'nombre1', 'apellido1', 'Lorem Ipsum1', 'Lorem Ipsum1', 'Lorem Ipsum1', 'Lorem Ipsum1', '(+58) 123456121', 'Lorem Ipsum@hotmail.com1', 'profile1.png', '$2y$10$EvcZZiJAdJiOkiaEHGDGr.8K8XvqHjYv/nzGKM3SmKxdP45QmFE6e1', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th1', 0, NULL, NULL, NULL),
-(70, 'alias2', 'nombre2', 'apellido2', 'Lorem Ipsum2', 'Lorem Ipsum2', 'Lorem Ipsum2', 'Lorem Ipsum2', '(+58) 123456122', 'Lorem Ipsum@hotmail.com2', 'profile2.png', '$2y$10$DXs1oSQHv03d70vNvfWgtuxG8RoKgq8T0ztbpZuVOgJhMI71xwQti2', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th2', 0, NULL, NULL, NULL),
-(71, 'alias3', 'nombre3', 'apellido3', 'Lorem Ipsum3', 'Lorem Ipsum3', 'Lorem Ipsum3', 'Lorem Ipsum3', '(+58) 123456123', 'Lorem Ipsum@hotmail.com3', 'profile3.png', '$2y$10$2vgHk9Lb071JUhk3./m/luIKsC9NYBhR/Sj50lFE27nnOFGiZqkw63', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th3', 0, NULL, NULL, NULL),
-(72, 'alias4', 'nombre4', 'apellido4', 'Lorem Ipsum4', 'Lorem Ipsum4', 'Lorem Ipsum4', 'Lorem Ipsum4', '(+58) 123456124', 'Lorem Ipsum@hotmail.com4', 'profile4.png', '$2y$10$3IWvcbTaJAn1as76XCXTEuzmzGMDqGSlnODsw.pq/YE0yFTCSSK424', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th4', 0, NULL, NULL, NULL),
-(73, 'alias5', 'nombre5', 'apellido5', 'Lorem Ipsum5', 'Lorem Ipsum5', 'Lorem Ipsum5', 'Lorem Ipsum5', '(+58) 123456125', 'Lorem Ipsum@hotmail.com5', 'profile5.png', '$2y$10$VBh5/NEzzG6l6FdIR2QLbuk526hIkIqa8.3Nq6TVfIXgRg3etAH4.5', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th5', 0, NULL, NULL, NULL),
-(74, 'alias6', 'nombre6', 'apellido6', 'Lorem Ipsum6', 'Lorem Ipsum6', 'Lorem Ipsum6', 'Lorem Ipsum6', '(+58) 123456126', 'Lorem Ipsum@hotmail.com6', 'profile6.png', '$2y$10$Ht1xoS0w2mYi4a7C5DfiieVBXlJVtn5PiWIWFgm5wibpTk9ENGdt66', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th6', 0, NULL, NULL, NULL);
+(60, 'Pablo', 'Pablo', 'Garcia', 'Lorem Ipsum', 'Lorem Ipsum', 'Lorem Ipsum', 'Lorem Ipsum', '(+58) 12345612', 'pablo@administrador.com', '16760398851.jpg', '$2y$10$I4bmnIwgVO0Cq2U6upMite1fSmQslFBCHQtC7Mi196N5SndkEYxWG', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th Lorem Ipsum has been the industry s standard dummy text ever since th0', 0, 'qMjuHmX9tyG93guQ3eHcuoDKCM61VuuNqybu17HvoSAMuz7R6kyB0rYkI90g', '2023-02-08 03:01:32', '2023-02-10 13:39:45'),
+(103, 'Lucia', 'Lucía', 'Martínez', 'Lorem Ipsum0', 'Lorem Ipsum0', 'Lorem Ipsum0', 'Lorem Ipsum0', '(+58) 123456120', 'administrador0@administrador.com', 'profile0.png', '$2y$10$118mTNWDjIm5vkZBQCuqK.5xetpn5DAM75dozHhMkZNINj4lrFa8S', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th Lorem Ipsum has been the industry s standard dummy text ever since th0', 1, 'Yl85hIGoxtBMiJ5h0twloKEE5JrbYLjvC4Xis07Zpiy2NeVXbAf0G69AASpY', NULL, '2023-02-10 20:02:30'),
+(104, 'Martina', 'Martina', 'Rodríguez', 'Lorem Ipsum1', 'Lorem Ipsum1', 'Lorem Ipsum1', 'Lorem Ipsum1', '(+58) 123456121', 'administrador1@administrador.com', 'profile1.png', '$2y$10$AotNFjy2MojrIUyAS5Pg0u2CtroZH44GU5Z2az0ZKGImc7kGwfYvW', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th Lorem Ipsum has been the industry s standard dummy text ever since th1', 1, 'XkHNelApLziY7URqQXNRk4EPyjnE5q2KFhbjJfVm1NyJXP75k511WYLCuuXl', NULL, '2023-02-10 19:55:29'),
+(105, 'Paula', 'Paula', 'Pérez', 'Lorem Ipsum2', 'Lorem Ipsum2', 'Lorem Ipsum2', 'Lorem Ipsum2', '(+58) 123456122', 'administrador2@administrador.com', 'profile2.png', '$2y$10$wqOgHyAzG/WhY9/bERM7QuLSNf31ESqdhM2a4JKgJjjuiWIn7nTou', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th Lorem Ipsum has been the industry s standard dummy text ever since th2', 0, 'hrUwDBN6n2JS4eCLOLBDajrGFSuI2gJfq8u2dYAPngTLPyQljbrye4X0096o', NULL, '2023-02-10 19:33:11'),
+(106, 'Lucas', 'Lucas', 'González', 'Lorem Ipsum3', 'Lorem Ipsum3', 'Lorem Ipsum3', 'Lorem Ipsum3', '(+58) 123456123', 'administrador3@administrador.com', 'profile3.png', '$2y$10$/KaFdQujj.J5D9f19mQUNevO4HZhtJZJQKfmCPMIQrFnhNKAXwnAe', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th Lorem Ipsum has been the industry s standard dummy text ever since th3', 1, 'WszPXKYP2TFz9DTyN8E4kFLfxCxZUZ6phPlGpGRst1rL7vb0VnsSZpyQmVnM', NULL, '2023-02-10 20:02:25'),
+(107, 'Marcos ', 'Sánchez', 'Muñoz', 'Lorem Ipsum4', 'Lorem Ipsum4', 'Lorem Ipsum4', 'Lorem Ipsum4', '(+58) 123456124', 'administrador4@administrador.com', 'profile4.png', '$2y$10$Fp7gb4JZNilbqWGQGU5G7Ol9Xxi/YActhMG6kXwH4NpU6bY9/kUbu', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th Lorem Ipsum has been the industry s standard dummy text ever since th4', 1, 'y5NBL9R8PlqTxiqQHHZsty6dqc1zq8uldGoaxxTcxMMsITfLwZKYkoUEweSS', NULL, '2023-02-10 08:33:17'),
+(108, 'Javier ', 'Hernández', 'Moreno', 'Lorem Ipsum5', 'Lorem Ipsum5', 'Lorem Ipsum5', 'Lorem Ipsum5', '(+58) 123456125', 'administrador5@administrador.com', 'profile5.png', '$2y$10$8Mzmg3tg6IsvBKJhanModuj5xHibQNZSJROWcrQRpsWWW0aGeZpB6', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th Lorem Ipsum has been the industry s standard dummy text ever since th5', 0, 'jFHCJlLKCHxyKKCJ9rKDVVVpZbOLfSvnv3XL3KhC8VpugINGHsoOnLYyuZoS', NULL, '2023-02-10 20:00:02'),
+(109, 'Alvaro', 'Guerrero', 'Jimenez', 'Lorem Ipsum6', 'Lorem Ipsum6', 'Lorem Ipsum6', 'Desarrollador deSoftware', '(+58) 123456126', 'administrador6@administrador.com', 'profile6.png', '$2y$10$Xaj2ZdNBJaRyNGbEDRFJi.8Fz0B5YCQf3ssKXxFvUqLFrDGNHzhgu', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since th Lorem Ipsum has been the industry s standard dummy text ever since th6', 1, 'hiQvbPueTGcd1CUIiiLeEx6jcucNI9Rc87X38lW7Iy2iAB403xWHsiX3VLWV', NULL, '2023-02-10 08:33:20');
 
 --
 -- Índices para tablas volcadas
@@ -301,25 +301,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=457;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `followers`
 --
 ALTER TABLE `followers`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=951;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `personal_access_tokens`
@@ -331,13 +331,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT de la tabla `publications`
 --
 ALTER TABLE `publications`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 
 --
 -- Restricciones para tablas volcadas
